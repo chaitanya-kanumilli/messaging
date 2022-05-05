62052c2e7fd903409f2e1084ede84bfc74134024
chaitanya kanumilli <chaitanya.k989@gmail.com>
2022-05-05 19:36:08 +0530

    initial changes

---
+++ Messaging/RabbitMqClient/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RabbitMqClient
+{
+    public class Class1
+    {
+    }
+}
---
+++ Messaging/messaging/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace messaging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
